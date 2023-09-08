3b19e0f752426f97165ec989575b509a90209394
SirPhoros <cgtorrubia@gmail.com>
2023-09-08 14:29:38 +0100

    create calculator in Python

---
+++ calculator/calculator.py
@@ -0,0 +1,21 @@
+def calculator():
+    num1 = float(input("Enter the first number: "))
+    operator = input("Enter an operator (+, -, *, /): ")
+    num2 = float(input("Enter the second number: "))
+    
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        result = num1 / num2
+    else:
+    
+        return
+    
+    print(f"Result: {num1} {operator} {num2} = {result}")
+
+# Call the calculator function to start the calculation
+calculator()
