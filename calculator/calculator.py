184fb1b4e51c2dd005e8e84efad4626bb76ea4b0
SirPhoros <cgtorrubia@gmail.com>
2023-09-08 14:31:59 +0100

    use ErrorHandlign functions to express the errors that may appear in the app

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,24 +1,30 @@
 def calculator():
-    num1 = float(input("Enter the first number: "))
-    operator = input("Enter an operator (+, -, *, /): ")
-    num2 = float(input("Enter the second number: "))
-    
-    if operator == "+":
-        result = num1 + num2
-    elif operator == "-":
-        result = num1 - num2
-    elif operator == "*":
-        result = num1 * num2
-    elif operator == "/":
-        if num2 == 0:
-            print("Error: Division by zero is not allowed.")
+    try:
+        num1 = float(input("Enter the first number: "))
+        operator = input("Enter an operator (+, -, *, /): ")
+        num2 = float(input("Enter the second number: "))
+        
+        if operator == "+":
+            result = num1 + num2
+        elif operator == "-":
+            result = num1 - num2
+        elif operator == "*":
+            result = num1 * num2
+        elif operator == "/":
+            if num2 == 0:
+                print("Error: Division by zero is not allowed.")
+                return
+            result = num1 / num2
+        else:
+            print("Invalid operator")
             return
-        result = num1 / num2
-    else:
-        print("Invalid operator")
-        return
+        
+        print(f"Result: {num1} {operator} {num2} = {result}")
     
-    print(f"Result: {num1} {operator} {num2} = {result}")
+    except ValueError:
+        print("Invalid input. Please enter valid numbers.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 # Call the calculator function to start the calculation
 calculator()
