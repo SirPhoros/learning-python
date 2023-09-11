8aee8d88f95fa039bc073c5609dd38aee7a02444
SirPhoros <cgtorrubia@gmail.com>
2023-09-11 22:17:55 +0100

    add user counter to check how many attempts it took to guess the correct number

--- guess-the-number/user-guess-number.py
+++ guess-the-number/user-guess-number.py
@@ -3,12 +3,20 @@ import random
 def guess(x):
     random_number = random.randint(1, x)
     number_guessed = 0
+    guess_count = 0
+
     while number_guessed != random_number:
-        number_guessed = int(input(f'Guess a number between 1 and {x}: '))
-        print(number_guessed)
-        if number_guessed < random_number:
-            print('Sorry, guess again. Too low. Try higher!')
-        elif number_guessed > random_number:
-            print('Sorry, guess again. Too high. lower!')
+            guess_count += 1
+            number_guessed = int(input(f'Guess a number between 1 and {x}: '))
+
+            if number_guessed < random_number:
+                print('Sorry, guess again. Too low. Try higher!')
+            elif number_guessed > random_number:
+                print('Sorry, guess again. Too high. Try lower!')
 
     print(f'Congratulations! You guessed the correct number, which is {random_number}.')
+    print(f'You took {guess_count} guesses.')
+
+
+# Call the guess function to start the game
+guess(10)
