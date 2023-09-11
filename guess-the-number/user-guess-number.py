dfaa348c3b76c0d90243ce09b63fa1744d5d63e2
SirPhoros <cgtorrubia@gmail.com>
2023-09-11 22:10:34 +0100

    implement while loop to keep guessing the number

--- guess-the-number/user-guess-number.py
+++ guess-the-number/user-guess-number.py
@@ -1,1 +1,10 @@
 import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    number_guessed = 0
+    while number_guessed != random_number:
+        number_guessed = int(input(f'Guess a number between 1 and {x}: '))
+        print(number_guessed)
+
+guess(10)    
