b51fa7c0e906c08b0e70971b55da36b76adfed30
SirPhoros <cgtorrubia@gmail.com>
2023-09-12 14:10:59 +0100

    catch error if wrong input is introduced

--- guess-the-number/computer-guess-number.py
+++ guess-the-number/computer-guess-number.py
@@ -10,20 +10,24 @@ def computer_guess(number):
         if low != high:
             guess = random.randint(low, high)
         else:
-            guess = low # could be also be high as high = low.
-        feedback = input(
-            f"Is {guess} too high (H), too Low (L), or correct (C)? "
-        ).lower()
-        if feedback == 'h':
+            guess = low
+
+        while feedback not in ("h", "l", "c"):
+            feedback = input(
+                f"Is {guess} too high (H), too low (L), or correct (C)? "
+            ).lower()
+
+        if feedback == "h":
             high = guess - 1
-        elif feedback == 'l':
+        elif feedback == "l":
             low = guess + 1
 
-    print(f'Yay! The computer guessed your number, {guess}, correctly!')  
+    print(f"Yay! The computer guessed your number, {guess}, correctly!")
 
-    play_again = input('Do you want to play again? (yes/no): ').lower()
-    if play_again == 'yes':
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again == "yes":
         computer_guess(number)
 
 
+# Call the computer_guess function to start the game
 computer_guess(10)
