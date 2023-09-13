466fe4965e248e682ca2c24c591a06b66b8b7cb5
SirPhoros <cgtorrubia@gmail.com>
2023-09-13 19:39:59 +0100

    declare function that chooses a random word from the list that does not include a '-' or a space

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,4 +1,9 @@
 import random
 from words import words
 
-print(words)
+def get_valid_word(words):
+    word = random.choice(words) #randomly chooses something from the list provided
+    while '-' in word or ' ' in word:
+        word = random.choice(words) 
+
+        return words.upper()
