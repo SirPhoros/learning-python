740ec9190864bdf495230b0aedc8526f8a613182
SirPhoros <cgtorrubia@gmail.com>
2023-09-13 19:55:56 +0100

    provides feedback to the user by saying what letters they have used and what letters are in the word

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,30 +1,41 @@
 import random
 from words import words
-import string 
+import string
+
 
 def get_valid_word(words):
-    word = random.choice(words) #randomly chooses something from the list provided
-    while '-' in word or ' ' in word:
-        word = random.choice(words) 
+    word = random.choice(words)  # randomly chooses something from the list provided
+    while "-" in word or " " in word:
+        word = random.choice(words)
 
-        return words.upper()
+    return word.upper()
+
 
 def hangman():
     word = get_valid_word(words)
-    word_letters = set(word) # letters in the word
+    word_letters = set(word)  # letters in the word
 
     alphabet = set(string.ascii_uppercase)
-    used_letters = set() # keep track of what the user has guessed
+    used_letters = set()  # keep track of what the user has guessed
+
 
     # getting user input
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters: 
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
-    
-    elif user_letter in used_letters:
-        print('You have already used that character. Please try again.')
+    while len(word_letters) > 0:
+        # letters used
+        print("You have used therese letters: ", " ".join(used_letters))
 
-    else: 
-        print('Invalid character. Please try again')
+        # what current word is:
+        word_list = [letter if letter in used_letters else "-" for letter in word]
+        print("Current word: ", " ".join(word_list))
+
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print("You have already used that character. Please try again.")
+
+        else:
+            print("Invalid character. Please try again")
