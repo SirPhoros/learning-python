5b94398940fd29565e65f08cc4bdc4b8e185d027
SirPhoros <cgtorrubia@gmail.com>
2023-09-14 15:07:18 +0100

    function to return available moves

--- tic-tac-toe/tic-tac-toe.py
+++ tic-tac-toe/tic-tac-toe.py
@@ -12,3 +12,10 @@ class TicTacToe:
         number_board = [[str(i) for i in range(j * 3, (j + 1) * 3)] for j in range(3)]
         for row in number_board:
             print("| " + " | ".join(row) + " |")
+
+    def available_moved(self):
+        moves = []
+        for i, spot in enumerate(self.board):
+            if spot == " ":
+                moves.append(i)
+        return moves
