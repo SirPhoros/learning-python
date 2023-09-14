0f5428ccb4ae9da18b3509031eb93809dd2bcf0c
SirPhoros <cgtorrubia@gmail.com>
2023-09-14 14:58:33 +0100

    create Player class

---
+++ tic-tac-toe/player.py
@@ -0,0 +1,11 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        # letter is X or O as in Tic-Tac-Toe
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
